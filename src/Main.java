09af3bbd11c34f0138c80eabb6069905e787b340
punkhye <hristina.cvetanova91@gmail.com>
2025-03-11 20:09:19 +0200

    commit

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,17 @@ import command.Viewer;
 public class Main {
     public static void main(String[] args) {
         Trainer trainer = new Trainer("gosho");
-        Viewer viewer = new Viewer("ediniq");
-        Viewer viewer2 = new Viewer("drugiq");
-        Viewer viewer3 = new Viewer("nqkoi");
-        Viewer viewer4 = new Viewer("toz ne trqq e tam");
+        Viewer viewer = new Viewer("viewer1");
+        Viewer viewer2 = new Viewer("viewer2");
+        Viewer viewer3 = new Viewer("viewer3");
 
         trainer.addViewer(viewer);
         trainer.addViewer(viewer2);
         trainer.addViewer(viewer3);
 
         trainer.GetUp();
+
+        trainer.LieDown();
 
     }
 }
